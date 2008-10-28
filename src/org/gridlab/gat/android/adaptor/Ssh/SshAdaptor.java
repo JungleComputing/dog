db5e16f1bf67b0bc5bcad76c2304a55aebb4d796
Roelof Kemp <r.kemp@cs.vu.nl>
2008-10-28 15:51:35 +0000

    Initial import.

---
+++ src/org/gridlab/gat/android/adaptor/Ssh/SshAdaptor.java
@@ -0,0 +1,13 @@
+package org.gridlab.gat.android.adaptor.Ssh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SshAdaptor extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
