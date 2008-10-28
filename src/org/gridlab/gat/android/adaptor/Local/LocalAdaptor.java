542be94e2f2743cf028a0e760d47d58410f4e136
Roelof Kemp <r.kemp@cs.vu.nl>
2008-10-28 15:50:51 +0000

    Initial import.

---
+++ src/org/gridlab/gat/android/adaptor/Local/LocalAdaptor.java
@@ -0,0 +1,13 @@
+package org.gridlab.gat.android.adaptor.Local;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocalAdaptor extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
