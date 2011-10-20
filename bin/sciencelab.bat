8941ed3868494e7d470cc095f390189e21cd02c5
Niels Drost <n.drost@esciencecenter.nl>
2011-10-20 14:33:51 +0000

    updated deploy, ipl, and some scripts to match

--- bin/sciencelab.bat
+++ bin/sciencelab.bat
@@ -6,7 +6,7 @@ rem %~dp0 is expanded pathname of the current script under NT
 
 if "%DEPLOY_HOME%X"=="X" set DEPLOY_HOME=%~dp0..\deploy
 
-set DEPLOY_ARGS=
+set DEPLOY_ARGS=sciencelab-workspace -r -m
 
 :setupArgs
 if ""%1""=="""" goto doneStart
@@ -16,8 +16,11 @@ goto setupArgs
 
 :doneStart
 
-cd %~dp0..
-
-java -classpath "%CLASSPATH%;%DEPLOY_HOME%;%DEPLOY_HOME%\lib\*;%DEPLOY_HOME%\lib\castor-1.1.1-xml.jar" -Djava.library.path="%DEPLOY_HOME%"\lib\natives -Dgat.adaptor.path="%DEPLOY_HOME%"\lib\adaptors -Dibis.deploy.home="%DEPLOY_HOME%" ibis.deploy.gui.GUI sciencelab-workspace -r %DEPLOY_ARGS%
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto 64BIT_ARCHITECTURE
+java -splash:images\ibis-splash.png -classpath "%CLASSPATH%;%DEPLOY_HOME%;%DEPLOY_HOME%\lib\*;%DEPLOY_HOME%\lib\castor-1.1.1-xml.jar" -Dgat.adaptor.path="%DEPLOY_HOME%"\lib\adaptors -Dibis.deploy.home="%DEPLOY_HOME%" -Djava.library.path="%DEPLOY_HOME%"\lib\native_libraries32 ibis.deploy.gui.GUI %DEPLOY_ARGS%
+goto END_ARCHITECTURE
+:64BIT_ARCHITECTURE
+java -splash:images\ibis-splash.png -classpath "%CLASSPATH%;%DEPLOY_HOME%;%DEPLOY_HOME%\lib\*;%DEPLOY_HOME%\lib\castor-1.1.1-xml.jar" -Dgat.adaptor.path="%DEPLOY_HOME%"\lib\adaptors -Dibis.deploy.home="%DEPLOY_HOME%" -Djava.library.path="%DEPLOY_HOME%"\lib\native_libraries ibis.deploy.gui.GUI %DEPLOY_ARGS%
+:END_ARCHITECTURE
 
 if "%OS%"=="Windows_NT" @endlocal
